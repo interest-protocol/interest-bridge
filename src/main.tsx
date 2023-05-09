5c5a3b6dbe42b94799216c6898be591a4babb491
git-marcopitra <marcomorete73@hotmail.com>
2023-05-09 06:13:24 +0100

    :green_heart: build: fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
-
 import './i18n';
+
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
